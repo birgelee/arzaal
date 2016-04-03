5e14ebcf706cb9ee24e035af659072bca4f7f94a
Richard Du <rdu@princeton.edu>
2016-04-03 00:33:43 -0400

    Deleted proximity service.

--- Arzaal/app/src/main/java/com/arzaal/arzaal/SharingScreen.java
+++ Arzaal/app/src/main/java/com/arzaal/arzaal/SharingScreen.java
@@ -1,34 +1,19 @@
 package com.arzaal.arzaal;
 
 import android.Manifest;
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.CheckBox;
-import android.widget.CompoundButton;
-import android.widget.RadioGroup;
-import android.widget.Toast;
 
 import com.arzaal.arzaal.contact.Contact;
-import com.arzaal.arzaal.contact.ContactSyncSettings;
-import com.arzaal.arzaal.proximity.ProximityService;
 import com.arzaal.arzaal.systemread.SystemReader;
 import com.arzaal.arzaal.systemupdate.SystemUpdater;
 
 public class SharingScreen extends AppCompatActivity {
 
     final private int REQUEST_CODE_ASK_PERMISSIONS = 123;
-
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,23 +22,16 @@ public class SharingScreen extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-
-
-
         askForPermissionAndWait(new String[]{Manifest.permission.WRITE_CONTACTS, Manifest.permission.READ_PHONE_STATE, Manifest.permission.GET_ACCOUNTS});
 
-        startService(new Intent(this, ProximityService.class));
         Contact c = SystemReader.readSystemContact(SettingsManager.manageSettings(this), this);
 
-
         SystemUpdater.addContactToSystem(c, this);
-        startService(new Intent(this, ProximityService.class));
 
     }
 
     @Override
     protected void onDestroy() {
-        stopService(new Intent(this, ProximityService.class));
         super.onDestroy();
     }
 
